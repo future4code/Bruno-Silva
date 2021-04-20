63b7677cf4b83f6a0bcb9a72d4cb4759dfa2b622
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-04-20 13:58:04 -0300

    criacao arquivo aula11

---
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -0,0 +1,75 @@
+//Exercícios de interpretação de código
+// Questão 1)
+/*  a. false
+    b. false
+    c. true
+    e. boolean
+*/
+
+// Questão 2)
+/*  a. undefined
+    b. object
+    c. 11
+    d. 3
+    e. [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    f. 9
+*/
+
+//Exercícios de escrita de código
+// Questão 1)
+// letra a)
+    /*let idadeUsuario
+    idadeUsuario = Number(prompt("Qual é a sua idade?"))
+// letra b)
+    let idadeMelhorAmigoUsuario
+    idadeMelhorAmigoUsuario = Number(prompt("Qual é a idade do(a) seu/sua melhor amigo(a)?"))
+// letra c)
+    let comparacaoIdades
+    comparacaoIdades = Boolean(idadeUsuario > idadeMelhorAmigoUsuario)
+    console.log("Sua idade é maior que seu(sua) amigo(a)?", comparacaoIdades)
+// letra d)
+    let diferencaIdades
+    diferencaIdades = idadeUsuario - idadeMelhorAmigoUsuario
+    console.log("A diferença de idades entre vocês é de", diferencaIdades, "anos!")*/
+
+// Questão 2)
+// letra a)
+    /*let numeroUsuario
+    numeroUsuario = Number(prompt("Digite um número par"))
+// letra b)
+    let restoDivisaoNumeroUsuario
+    restoDivisaoNumeroUsuario = numeroUsuario % 2
+    console.log("O resto da divisão do seu número por 2 é:", restoDivisaoNumeroUsuario)*/
+/* letra c) Observa-se que ao fazer o resto da divisao de um número qualquer por 2 os
+únicos possíveis resultados são 0 ou 1.*/
+/* letra d) Se o usuário inserir um número ímpar, o resto da divisão desse número
+por 2 sempre será igual a 1, enquanto para os números pares sempre igual a 0.*/
+
+// Questão 3)
+// letra a)
+    /*let listaDeTarefas = []
+// letra b)
+    listaDeTarefas[0] = prompt("Diga a 1º tarefa do seu dia")
+    listaDeTarefas[1] = prompt("Diga a 2º tarefa do seu dia")
+    listaDeTarefas[2] = prompt("Diga a 3º tarefa do seu dia")
+// letra c)
+    console.log(listaDeTarefas)
+// letra d)
+    let indiceListaDeTarefas
+    indiceListaDeTarefas = Number(prompt("Digite o índice da tarefa realizada (0 , 1 ou 2)"))
+// letra e)
+    listaDeTarefas.splice(indiceListaDeTarefas,1)
+// letra f)
+    console.log(listaDeTarefas)*/
+
+// Questão 4)
+    /*let nomeDoUsuario
+    let emailDoUsuario
+    let mensagemFinal
+
+    nomeDoUsuario = prompt("Digite seu nome:")
+    emailDoUsuario = prompt("Digite seu e-mail:")
+
+    mensagemFinal = `O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`
+
+    console.log(mensagemFinal)*/

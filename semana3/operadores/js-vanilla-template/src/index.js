7801cbced32f3e744fbbe8351d22331f0cf13c3b
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-04-20 16:12:31 -0300

    alteracao pontual aula11

--- semana3/operadores/js-vanilla-template/src/index.js
+++ semana3/operadores/js-vanilla-template/src/index.js
@@ -18,28 +18,28 @@
 //Exercícios de escrita de código
 // Questão 1)
 // letra a)
-    /*let idadeUsuario
-    idadeUsuario = Number(prompt("Qual é a sua idade?"))
+    /*let idadeUsuario;
+    idadeUsuario = Number(prompt("Qual é a sua idade?"));
 // letra b)
-    let idadeMelhorAmigoUsuario
-    idadeMelhorAmigoUsuario = Number(prompt("Qual é a idade do(a) seu/sua melhor amigo(a)?"))
+    let idadeMelhorAmigoUsuario;
+    idadeMelhorAmigoUsuario = Number(prompt("Qual é a idade do(a) seu/sua melhor amigo(a)?"));
 // letra c)
-    let comparacaoIdades
-    comparacaoIdades = Boolean(idadeUsuario > idadeMelhorAmigoUsuario)
-    console.log("Sua idade é maior que seu(sua) amigo(a)?", comparacaoIdades)
+    let comparacaoIdades;
+    comparacaoIdades = Boolean(idadeUsuario > idadeMelhorAmigoUsuario);
+    console.log("Sua idade é maior que seu(sua) amigo(a)?", comparacaoIdades);
 // letra d)
-    let diferencaIdades
-    diferencaIdades = idadeUsuario - idadeMelhorAmigoUsuario
-    console.log("A diferença de idades entre vocês é de", diferencaIdades, "anos!")*/
+    let diferencaIdades;
+    diferencaIdades = idadeUsuario - idadeMelhorAmigoUsuario;
+    console.log("A diferença de idades entre vocês é de", diferencaIdades, "anos!");*/
 
 // Questão 2)
 // letra a)
-    /*let numeroUsuario
-    numeroUsuario = Number(prompt("Digite um número par"))
+    /*let numeroUsuario;
+    numeroUsuario = Number(prompt("Digite um número par"));
 // letra b)
-    let restoDivisaoNumeroUsuario
-    restoDivisaoNumeroUsuario = numeroUsuario % 2
-    console.log("O resto da divisão do seu número por 2 é:", restoDivisaoNumeroUsuario)*/
+    let restoDivisaoNumeroUsuario;
+    restoDivisaoNumeroUsuario = numeroUsuario % 2;
+    console.log("O resto da divisão do seu número por 2 é:", restoDivisaoNumeroUsuario);*/
 /* letra c) Observa-se que ao fazer o resto da divisao de um número qualquer por 2 os
 únicos possíveis resultados são 0 ou 1.*/
 /* letra d) Se o usuário inserir um número ímpar, o resto da divisão desse número
@@ -47,29 +47,29 @@ por 2 sempre será igual a 1, enquanto para os números pares sempre igual a 0.*
 
 // Questão 3)
 // letra a)
-    /*let listaDeTarefas = []
+    /*let listaDeTarefas = [];
 // letra b)
-    listaDeTarefas[0] = prompt("Diga a 1º tarefa do seu dia")
-    listaDeTarefas[1] = prompt("Diga a 2º tarefa do seu dia")
-    listaDeTarefas[2] = prompt("Diga a 3º tarefa do seu dia")
+    listaDeTarefas[0] = prompt("Diga a 1º tarefa do seu dia");
+    listaDeTarefas[1] = prompt("Diga a 2º tarefa do seu dia");
+    listaDeTarefas[2] = prompt("Diga a 3º tarefa do seu dia");
 // letra c)
-    console.log(listaDeTarefas)
+    console.log(listaDeTarefas);
 // letra d)
-    let indiceListaDeTarefas
-    indiceListaDeTarefas = Number(prompt("Digite o índice da tarefa realizada (0 , 1 ou 2)"))
+    let indiceListaDeTarefas;
+    indiceListaDeTarefas = Number(prompt("Digite o índice da tarefa realizada (0 , 1 ou 2)"));
 // letra e)
-    listaDeTarefas.splice(indiceListaDeTarefas,1)
+    listaDeTarefas.splice(indiceListaDeTarefas,1);
 // letra f)
-    console.log(listaDeTarefas)*/
+    console.log(listaDeTarefas);*/
 
 // Questão 4)
-    /*let nomeDoUsuario
-    let emailDoUsuario
-    let mensagemFinal
+    /*let nomeDoUsuario;
+    let emailDoUsuario;
+    let mensagemFinal;
 
-    nomeDoUsuario = prompt("Digite seu nome:")
-    emailDoUsuario = prompt("Digite seu e-mail:")
+    nomeDoUsuario = prompt("Digite seu nome:");
+    emailDoUsuario = prompt("Digite seu e-mail:");
 
-    mensagemFinal = `O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`
+    mensagemFinal = `O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`;
 
-    console.log(mensagemFinal)*/
+    console.log(mensagemFinal);*/

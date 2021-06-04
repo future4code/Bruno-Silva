42f2ecaed87129e1ffea8eada90babbdb5918fe2
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-06-04 15:25:31 -0300

    ajuste fino estilizacao

--- semana9/astromatch-project/src/Components/CrushesProfile/CrushesProfileStyles.js
+++ semana9/astromatch-project/src/Components/CrushesProfile/CrushesProfileStyles.js
@@ -10,7 +10,7 @@ export const CrushContainer = styled.div`
     width: 90%;
     margin: auto;
     border: 1px solid yellowgreen;
-    border-radius: 8px;
+    border-radius: 4px;
     box-shadow: 2px 2px 2px darkgrey;
     display: grid;
     grid-template-rows: 2fr 1fr;
@@ -20,12 +20,15 @@ export const PhotoProfile = styled.div`
     background-image: url(${(props) => props.photoProfile});
     background-size: 100% 100%;
     background-repeat: no-repeat;
+    border-radius: 4px;
+    margin-top: -1px;
 `
 
 export const InfoBox = styled.div`
     color: white;
     background-image: linear-gradient(#000000, #55efc4);
     padding: 8px;
+    margin-top: -1px;
     display: grid;
     grid-template-rows: 1fr 1fr 1fr;
 

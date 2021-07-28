2a5589b90d6a73ee60582d79f8d39fa404e81a3c
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-07-28 18:20:07 -0300

    ajustes

--- semana17/aula51/src/endpoints/createUserAddress.ts
+++ semana17/aula51/src/endpoints/createUserAddress.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import connection from "../data/connection";
 import getAddressInfo from "../services/getAddressInfo";
+import transporter from "../services/mailTransporter";
 import { addressInfo, userAddress } from "../types";
 
 export default async function createUserAddress(
@@ -19,28 +20,38 @@ export default async function createUserAddress(
 
       const result: addressInfo | null = await getAddressInfo(zipcode);
 
-      if (!result) {
-         errorCode = 500;
-         throw new Error("Ops! Ocorreu um erro inesperado :/");
-
-      } else if (result) {
+      if (result) {
          const { street, neighbourhood, city, state } = result;
+
+         if (!street || !neighbourhood || !city || !state) {
+            errorCode = 422;
+            throw new Error(`Zipcode doesn´t exist! Please, check again!`);
+         };
          
          const newUser: userAddress = { zipcode, street, numberAdd, complement, neighbourhood, city, state };
 
          await connection('UsersAddress')
             .insert(newUser);
+   
+         process.env.NODEMAILER_USER ? await transporter.sendMail({
+            from: `<${process.env.NODEMAILER_USER}>`,
+            to: `<g6e8k2i3m1o7e5d9@labenualunos.slack.com>`,
+            subject: "Desafio-Aula51",
+            text: `
+               O objeto utilizado foi:
 
-         res.status(201).send("Endereço de usuário criado com sucesso!")
+               await transporter.sendMail({;
+                  from: email_do_usuario,
+                  to: email_labenu,
+                  subject: "Desafio-Aula51",
+                  text: texto digitado
+               })
+            `
+         }) : null;
+
+         res.status(201).send("Endereço de usuário criado com sucesso!");
       }
    } catch (error) {
-      if (typeof error === "string") {
-
-         res.status(errorCode).send(error)
-      } else {
-
-         console.log(error.sqlMessage || error.message);
-         res.status(500).send("Ops! Ocorreu um erro inesperado :/");
-      };
+      res.status(errorCode).send({ message: error.sqlMessage || error.message });
    };
 };

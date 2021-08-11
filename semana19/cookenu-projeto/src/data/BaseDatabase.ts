9591f6c783d6dcd934ed3f176e673d526f18332e
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-08-11 14:56:19 -0300

    configurando app, connection e basedatabase

---
+++ semana19/cookenu-projeto/src/data/BaseDatabase.ts
@@ -0,0 +1,18 @@
+import knex from 'knex';
+import Knex from "knex";
+
+class BaseDatabase {
+    protected static connection: Knex = knex({
+        client: "mysql",
+        connection: {
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT || "3306"),
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_SCHEMA,
+            multipleStatements: true
+        }
+    });
+};
+
+export default BaseDatabase;

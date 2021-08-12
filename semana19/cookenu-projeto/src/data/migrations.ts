0c8d19089aed7419a38062cf2593bc48a3f0525f
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-08-12 16:25:12 -0300

    ajustes no migrations quase pronto

--- semana19/cookenu-projeto/src/data/migrations.ts
+++ semana19/cookenu-projeto/src/data/migrations.ts
@@ -1,50 +1,73 @@
 import connection from "./connection";
 import users from './users.json';
 import recipes from './recipes.json';
+import { BaseDatabase } from "./BaseDatabase";
 
 const printError = (error: any) => { console.log(error.sqlMessage || error.message) };
 
-const createTables = () => connection
-    .raw(`
-        CREATE TABLE IF NOT EXISTS User_projeto19(
-	        id VARCHAR(255) PRIMARY KEY NOT NULL,
-            name VARCHAR(50) NOT NULL,
-            email VARCHAR(255) UNIQUE NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            role ENUM("ADMIN", "NORMAL") DEFAULT "NORMAL"
-        );
+class Migrations extends BaseDatabase {
+    public static createTables = async (): Promise<void> => {
+        await BaseDatabase.connection.raw(`
+                CREATE TABLE IF NOT EXISTS User(
+	                id VARCHAR(255) PRIMARY KEY NOT NULL,
+                    name VARCHAR(50) NOT NULL,
+                    email VARCHAR(255) UNIQUE NOT NULL,
+                    password VARCHAR(255) NOT NULL,
+                    role ENUM("ADMIN", "NORMAL") DEFAULT "NORMAL"
+                );
 
-        CREATE TABLE IF NOT EXISTS Recipe_projeto19(
-	        id VARCHAR(255) PRIMARY KEY NOT NULL,
-            title VARCHAR(50) NOT NULL,
-            description TEXT NOT NULL,
-            createdAt DATE NOT NULL,
-	        creatorId VARCHAR(255) NOT NULL,
-	        FOREIGN KEY (creatorId) REFERENCES User_projeto19(id)
-        );
+                CREATE TABLE IF NOT EXISTS Recipe(
+	                id VARCHAR(255) PRIMARY KEY NOT NULL,
+                    title VARCHAR(50) NOT NULL,
+                    description TEXT NOT NULL,
+                    createdAt DATE NOT NULL,
+	                creatorId VARCHAR(255) NOT NULL,
+	                FOREIGN KEY (creatorId) REFERENCES User(id)
+                );
 
-        CREATE TABLE IF NOT EXISTS FollowUser_junction(
-            followerUserId VARCHAR(255) NOT NULL,
-            followedUserId VARCHAR(255) NOT NULL,
-            PRIMARY KEY (followerUserId, followedUserId)
-        );
-    `)
-    .then(() => console.log("Tables created successfully!"))
-    .catch(printError);
+                CREATE TABLE IF NOT EXISTS FollowUser_junction(
+                    followerUserId VARCHAR(255) NOT NULL,
+                    followedUserId VARCHAR(255) NOT NULL,
+                    PRIMARY KEY (followerUserId, followedUserId),
+                    FOREIGN KEY (followerUserId) REFERENCES User(id),
+                    FOREIGN KEY (followedUserId) REFERENCES User(id)
+                );
+            `)
+            .then(() => console.log("Tables created successfully!"))
+            .catch(printError);
+    };
 
-const insertUsers = () => connection("User_projeto19")
-    .insert(users)
-    .then(() => console.log("Users created successfully!"))
-    .catch(printError);
+    public static insertUsers = async (data: any):Promise<void> => {
+        await BaseDatabase.connection("User")
+            .insert(data)
+            .then(() => console.log(`Users created successfully!`))
+            .catch(printError);
+    };
 
-const insertRecipes = () => connection("Recipe_projeto19")
-    .insert(recipes)
-    .then(() => console.log("Recipes created successfully!"))
-    .catch(printError);
+    public static insertRecipes = async (data: any):Promise<void> => {
+        await BaseDatabase.connection("Recipe")
+            .insert(data)
+            .then(() => console.log(`Recipes created successfully!`))
+            .catch(printError);
+    };
 
-const closeConnection = () => { connection.destroy()};
+    public static closeConnection():Promise<void> {
+        return BaseDatabase.connection.destroy();
+    }
+};
 
-createTables()
-    .then(insertUsers)
-    .then(insertRecipes)
-    .finally(closeConnection);
+const up = async() => {
+    try {
+        await Migrations.createTables();
+
+        await Migrations.insertUsers(users);
+
+        await Migrations.insertRecipes(recipes);
+    } catch(error) {
+        throw new Error(error.sqlMessage);
+    } finally {
+        Migrations.closeConnection();
+    }
+};
+
+up().then(res => console.log("Fim de criaçao!")).catch(error => console.log(error));
--- semana19/cookenu-projeto/src/endpoints/getRecipesOfFollowedUsers.ts
+++ semana19/cookenu-projeto/src/endpoints/getRecipesOfFollowedUsers.ts
@@ -4,16 +4,16 @@ import { RecipeDatabase } from '../data/RecipeDatabase';
 import { Recipe } from '../entities/Recipe';
 import { Authenticator } from '../services/Authenticator';
 
-const getRecipesOfFollowedUsers = async(
+const getRecipesOfFollowedUsers = async (
     req: Request,
     res: Response
-):Promise<void> => {
+): Promise<void> => {
     let errorCode: number = 400;
 
     try {
         const token = req.headers.authorization;
 
-        if(!token) {
+        if (!token) {
             errorCode = 422;
             throw new Error("Endpoint usage requires authorization! Please, try to insert a valid token");
         };
@@ -22,17 +22,28 @@ const getRecipesOfFollowedUsers = async(
         const follows = await FollowUserDatabase.getFollowByFollowerId(authData.id);
 
         let recipesFromFollows: Recipe[] = [];
-        for(let follow of follows){
+        for (let follow of follows) {
             const recipesByFollowed = await RecipeDatabase.getRecipesByCreatorId(follow.followedUserId);
 
-            for (let recipe of recipesByFollowed){
+            for (let recipe of recipesByFollowed) {
                 recipesFromFollows.push(recipe);
             };
         };
 
-        res.status(200).send({ recipes: recipesFromFollows});
-    } catch(error) {
-        res.status(errorCode).send({ message: error.message? error.message : error.sqlMessage });
+        const orderedRecipes = recipesFromFollows.sort(function (a, b) {
+            if (a.createdAt > b.createdAt) {
+                return 1;
+            }
+            if (a.createdAt < b.createdAt) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        });
+
+        res.status(200).send({ recipes: orderedRecipes });
+    } catch (error) {
+        res.status(errorCode).send({ message: error.message ? error.message : error.sqlMessage });
     }
 };
 

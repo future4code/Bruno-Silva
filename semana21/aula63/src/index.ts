823b8dd7dcaf4f4e648911e9a4e112f69f658ff7
BrunoSilva28 <brunosantosasilva@gmail.com>
2021-08-25 23:11:52 -0300

    finalizado atividades

--- semana21/aula63/src/index.ts
+++ semana21/aula63/src/index.ts
@@ -18,3 +18,32 @@ export const validateCharacter = (character: Character):boolean =>{
 
     return true;
 };
+
+// export const performAttack = (
+//     attacker: Character,
+//     defender: Character
+// ) => {
+//     if(!validateCharacter(attacker) || !validateCharacter(defender)){
+//         throw new Error("Invalid Character");
+//     };
+
+//     if(attacker.strength > defender.defense){
+//         defender.life = defender.life - attacker.strength + defender.defense;
+//     };
+// };
+
+export const performAttack = (
+    attacker: Character,
+    defender: Character,
+    validator: (input: Character) => boolean
+) => {
+    if(!validator(attacker) || !validator(defender)){
+        throw new Error("Invalid Character");
+    };
+
+    if(attacker.strength > defender.defense){
+        return defender.life = defender.life - attacker.strength + defender.defense;
+    };
+
+    return defender.life;
+};
---
+++ semana21/aula63/tests/mocks/characterMock.ts
@@ -0,0 +1,15 @@
+import { Character } from '../../src/index';
+
+export const attackerMockSuccess: Character = {
+    name: "Teste",
+    life: 1500,
+    strength: 300,
+    defense: 100
+};
+
+export const defenderMockSuccess: Character = {
+    name: "Teste",
+    life: 1500,
+    strength: 150,
+    defense: 100
+};
